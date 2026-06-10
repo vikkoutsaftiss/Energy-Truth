4edc438e5387373839c1a59162e60a5cff7ffc74
Vik Koutsaftis <241661@student.fontys.nl>
2026-06-10 13:10:40 +0200

    Add new calculator and converter files

---
+++ Energy-Truth-WEB-API/Calculators/IImportCalculator.cs
@@ -0,0 +1,13 @@
+﻿using Energy_Truth.Shared;
+using Energy_Truth.Shared.DTO_s;
+
+namespace Energy_Truth_WEB_API.Calculators
+{
+    public interface IImportCalculator
+    {
+        IEnumerable<UsageDataDTO> CalculateCumulativeImport(List<EnergyImportDTO> data, string provider);
+        IEnumerable<UsageDataDTO> MapDirectImport(List<EnergyImportDTO> data, string provider);
+         IEnumerable<UsageDataDTO> CalculateImport(List<EnergyImportDTO> data, string providerName);
+
+    }
+}
